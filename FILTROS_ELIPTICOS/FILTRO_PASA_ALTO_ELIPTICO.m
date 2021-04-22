ea32282d69e4a5499000bf386b0d05745e00af70
miguel miguel <miguef10hd@gmail.com>
2021-04-21 21:22:00 -0500

    Adición de Filtros Elipticos

---
+++ FILTROS_ELIPTICOS/FILTRO_PASA_ALTO_ELIPTICO.m
@@ -0,0 +1,50 @@
+info = audioinfo('Yabu_mono.wav')
+[y,Fs] = audioread('Yabu_mono.wav');
+t = 0:seconds(1/Fs):seconds(info.Duration);
+t = t(1:end-1);
+%figure,plot(t,y);
+%sound(y,Fs)
+
+%TRANSFORMADA DE FOURIER DE LA FORMA DE ONDA ORIGINAL
+Y=fft(y);
+L=length(y);
+P2=abs(Y/L);
+P1=P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+        %GRAFICA DE LA TRANSFORMADA DE FOURIER DE LA FORMA DE ONDA ORIGINAL
+        f = Fs*(0:(L/2))/L;
+        figure, plot(f,P1) 
+        title('Grafico de la transformada de Fourier de y(t)')
+        xlabel('f (Hz)')
+        ylabel('Magnitud de la TF')
+
+%DETERMINACION DE COEFICIENTES DEL FILTRO
+Wp=2000/(Fs/2);
+Ws=1500/(Fs/2);
+Rp=1;
+Rs=60;
+[n, Wn]=ellipord(Wp, Ws, Rp,Rs);
+
+%DEFINICION Y GRAFICO DE FILTRO PASABANDA ELIPTICO
+[b,a] = ellip(n,Rp,Rs,Wn, 'high');  %FRECUENCIA ESCOGIDA DE 301 A 5 KHz
+[H,w] = freqz(b,a,512);   %frecuencia del filtro w (512 muestras)  H complejo (512 muestras)
+size(w)
+size(H)
+figure, plot(w*Fs/(2*pi),abs(H));
+xlabel('Frecuencia (Hz)'); ylabel('Respuesta en Frecuencia');
+grid;
+        %FILTRO APLICADO AL AUDIO ORIGINAL
+        sf = filter(b,a,y);
+        SF= fft(sf);
+        L=length(y);
+        P2=abs(SF/L);
+        P1=P2(1:L/2+1);
+        P1(2:end-1) = 2*P1(2:end-1);
+            %GRAFICO DE FILTRO APLICADO AL AUDIO
+            f = Fs*(0:(L/2))/L;
+            figure, plot(f,P1) 
+            title('fILTRO PASABANDA ELIPTICO APLICADO')
+            xlabel('f (Hz)')
+            ylabel('Magnitud de la TF')
+            sound(sf,Fs)
+
